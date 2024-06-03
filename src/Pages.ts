adc25311cdf7f355a01b12f85a0a243ee031695c
Philip Zingmark <philip.zingmark@gmail.com>
2024-06-03 19:08:05 +0200

    Added bottom navbar and fixed formatting dependecy that was missing, also changed the Page type to separate icon and label

--- src/Pages.ts
+++ src/Pages.ts
@@ -2,26 +2,28 @@ import HealthData from './pages/health-data/HealthData';
 import Home from './pages/home/Home';
 import Sprint from './pages/sprint/Sprint';
 import { Page } from './types/page';
+import { HomeIcon, SprintIcon, Svg } from './utils/Icons';
+import healthdataicon from './assets/navigation/health-data.svg';
 
 export const pages: Page[] = [
   {
     to: '/health-data',
     permissionLevel: 0,
-    label: 'health-data',
+    label: () => Svg({ src: healthdataicon }), // Health data icon is an image and not svg :(
     isMenu: true,
     component: HealthData,
   },
   {
     to: '/',
     permissionLevel: 0,
-    label: 'home',
+    label: HomeIcon,
     isMenu: true,
     component: Home,
   },
   {
     to: '/sprint',
     permissionLevel: 0,
-    label: 'sprint',
+    label: SprintIcon,
     isMenu: true,
     component: Sprint,
   },
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import { Page } from '../types/page';
 import { pages } from '../Pages';
+import { styled } from '@mui/material';
 
 export interface NavigationProps {
   authLevel: number;
 }
 
 export function Navigation({ authLevel }: NavigationProps) {
+  const NavigationBar = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '20vw',
+    position: 'fixed',
+    bottom: '0',
+    left: '0',
+    width: '100%',
+    minHeight: '8vh',
+    backgroundColor: 'white',
+    zIndex: '1000',
+    'nav-link': {
+      color: 'inherit',
+    },
+    '.active': {
+      color: '#00A3E0',
+    },
+  });
+
   return (
-    <>
+    <NavigationBar>
       {pages.map(
         (page: Page) =>
           page.isMenu &&
@@ -18,6 +38,6 @@ export function Navigation({ authLevel }: NavigationProps) {
             </NavLink>
           )
       )}
-    </>
+    </NavigationBar>
   );
 }
