c6b929ee913e850bb3e7771e0d0a09411a0de7e6
Philip Zingmark <philip.zingmark@gmail.com>
2024-06-03 14:53:04 +0200

    added under construction asset and temporarily put it in the pages

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,10 @@
+import under_construction from '../../assets/popup/under-construction.svg';
+
 export default function Home() {
-  return <h1>Under construction</h1>;
+  return (
+    <div>
+      <h1>Under consruction</h1>
+      <img src={under_construction} />
+    </div>
+  );
 }
