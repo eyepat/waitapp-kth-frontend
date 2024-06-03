9200d8bbf592cca39726c2cab429c5f7fa8e8871
Philip Zingmark <philip.zingmark@gmail.com>
2024-06-03 14:54:03 +0200

    fix typo

--- src/pages/health-data/HealthData.tsx
+++ src/pages/health-data/HealthData.tsx
@@ -3,7 +3,7 @@ import under_construction from '../../assets/popup/under-construction.svg';
 export default function HealthData() {
   return (
     <div>
-      <h1>Under consruction</h1>
+      <h1>Under construction</h1>
       <img src={under_construction} />
     </div>
   );
