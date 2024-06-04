59b5492cc583c0ee2b7ce402bcfb85e26258eeb1
PatBtw <patkon101@gmail.com>
2024-06-04 14:49:16 +0200

    fix: Update Header component to improve layout and add navigation buttons
    The Header component was updated to improve the layout and add navigation buttons for back, closed book, and settings. The layout was adjusted using flexbox to position the buttons correctly. This change enhances the functionality and visual appeal of the header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import { styled } from '@mui/material';
-import { Svg } from '../utils/Icons';
+import { Button, styled } from '@mui/material';
+import { BackArrow, ClosedBook, Settings, Svg } from '../utils/Icons';
 import ki from '../assets/logo/ki.svg';
 
 export default function Header() {
   const TopBar = styled('div')({
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     gap: '20vw',
     position: 'sticky',
     top: '0',
@@ -21,9 +21,50 @@ export default function Header() {
       color: '#00A3E0',
     },
   });
+  const LeftSide = styled('div')({
+    display: 'flex',
+    justifyContent: 'start',
+    marginLeft: '5vw',
+    gap: '10vw',
+    alignItems: 'center',
+  });
+  const RightSide = styled('div')({
+    display: 'flex',
+    justifyContent: 'end',
+    marginRight: '5vw',
+    gap: '10vw',
+    alignItems: 'center',
+  });
+  const ButtonOverride = styled(Button)({
+    margin: '0',
+    padding: '0',
+    minWidth: '0',
+    color: 'inherit',
+  });
   return (
     <TopBar>
-      <Svg src={ki} />
+      <LeftSide>
+        <ButtonOverride>
+          <BackArrow />
+        </ButtonOverride>
+      </LeftSide>
+      <Svg
+        src={ki}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+      <RightSide>
+        <ButtonOverride>
+          <ClosedBook />
+        </ButtonOverride>
+        <ButtonOverride>
+          <Settings />
+        </ButtonOverride>
+      </RightSide>
     </TopBar>
   );
 }
