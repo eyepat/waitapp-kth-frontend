79cb1d6a2a3be8b0f2b3e56822982f4298fbbdbb
Philip Zingmark <philip.zingmark@gmail.com>
2024-06-05 01:27:17 +0200

    start on adding sections

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,5 +1,21 @@
-import { Stack, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { useLanguage } from '../../contexts/LanguageContext';
+import {
+  ArrowRightOutlined,
+  PersonOutline,
+  SafetyCheck,
+  SafetyCheckOutlined,
+} from '@mui/icons-material';
 
 export default function Settings() {
   const { t } = useLanguage();
@@ -10,10 +26,46 @@ export default function Settings() {
       padding="0 8vw 0 8vw"
       maxWidth="1000px"
       margin="auto"
+      spacing={3}
     >
       <Typography variant="h5" marginBottom="1rem" alignSelf="center">
         {t('settings')}
       </Typography>
+      <Card sx={{ width: '100%' }}>
+        <CardHeader title={t('profile')} />
+        <CardContent>
+          <Button
+            fullWidth={true}
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
+              <PersonOutline />
+              <Typography>{t('personal-details')}</Typography>
+            </div>
+            <ArrowRightOutlined />
+          </Button>
+          <Button
+            fullWidth={true}
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
+              <SafetyCheck />
+              <Typography>{t('password-and-security')}</Typography>
+            </div>
+            <ArrowRightOutlined />
+          </Button>
+          <Button
+            fullWidth={true}
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
+              <SafetyCheckOutlined />
+              <Typography>{t('integrity')}</Typography>
+            </div>
+            <ArrowRightOutlined />
+          </Button>
+        </CardContent>
+      </Card>
     </Stack>
   );
 }
