982e006c7a44e9a70015aecb8d8a75fdd05fde79
PatBtw <patkon101@gmail.com>
2024-06-04 17:19:58 +0200

    feat: Update fonts and add dashboard components
    The code changes include updating the fonts in the CSS file to use the 'Open Sans' font family and adding new components for the dashboard. The 'Home' component now displays dynamic data such as the number of days left, weight, steps, and pulse. The translations for the 'en.json' and 'sv.json' files were also updated to include new keys for the dashboard data.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,55 @@
-import under_construction from '../../assets/popup/under-construction.svg';
+import { Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useLanguage } from '../../contexts/LanguageContext';
+import { Pulse, Scale, Steps } from '../../utils/Icons';
 
 export default function Home() {
+  const [daysLeft, setDaysLeft] = useState<number>(142);
+  const [weight, setWeight] = useState<number>(84);
+  const [steps, setSteps] = useState<number>(2472);
+  const [pulse, setPulse] = useState<number>(74);
+  const { t } = useLanguage();
   return (
-    <div>
-      <h1>Under construction</h1>
-      <img src={under_construction} />
+    <div style={{ width: '85%', margin: 'auto', fontFamily: 'Open Sans' }}>
+      <Stack direction="column">
+        <Stack direction="row" sx={{ width: '60%', alignItems: 'center' }}>
+          <Typography variant="h2" className="open-sans-bold" color="#00A3E0">
+            {daysLeft}
+          </Typography>
+
+          <Typography variant="h5" sx={{ lineHeight: '1', fontWeight: 'bold' }}>
+            {t('days-left')}
+          </Typography>
+        </Stack>
+        <Stack direction="column" spacing={2}>
+          <Typography variant="caption" lineHeight={3}>
+            {t('latest-result')}
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Scale />
+            <Stack direction="column">
+              <Typography sx={{ lineHeight: '1' }}>{weight}</Typography>
+              <Typography sx={{ lineHeight: '1' }}>
+                {t('weight-unit')}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Steps />
+            <Stack direction="column">
+              <Typography sx={{ lineHeight: '1' }}>{steps}</Typography>
+              <Typography sx={{ lineHeight: '1' }}>{t('steps')}</Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Pulse />
+            <Stack direction="column">
+              <Typography sx={{ lineHeight: '1' }}>{pulse}</Typography>
+              <Typography sx={{ lineHeight: '1' }}>{t('pulse')}</Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 }
