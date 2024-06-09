6afe8f98c923e8d1adb3ed817fbdec2023925f4c
PatBtw <patkon101@gmail.com>
2024-06-09 18:12:10 +0200

    Early version of the chart bar. (lots to fix)

---
+++ src/pages/home/ChartMeter.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+interface ChartMeterProps {
+  value: number;
+  label: any;
+  barColor: string;
+  lighterColor: string;
+  maxValue: number;
+}
+
+const ChartMeter: React.FC<ChartMeterProps> = ({
+  value,
+  label,
+  barColor,
+  lighterColor,
+  maxValue,
+}) => {
+  const height = `${(value / maxValue) * 300}px`;
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="body1" color="textSecondary">
+        {label}
+      </Typography>
+      <Box
+        width="15px"
+        height="400px"
+        bgcolor={lighterColor}
+        borderRadius="15px"
+        position="relative"
+        marginTop="8px"
+      >
+        <Box
+          position="absolute"
+          bottom="0"
+          width="100%"
+          height={height}
+          bgcolor={barColor}
+          borderRadius="inherit"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChartMeter;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,30 @@
+import React, { useState } from 'react';
 import { Stack, Typography } from '@mui/material';
-import { useState } from 'react';
-import { useLanguage } from '../../contexts/LanguageContext';
 import { Pulse, Scale, Steps } from '../../utils/Icons';
+import ChartMeter from './ChartMeter';
+import { useLanguage } from '../../contexts/LanguageContext';
 
-export default function Home() {
+const Home: React.FC = () => {
   const [daysLeft, setDaysLeft] = useState<number>(142);
   const [weight, setWeight] = useState<number>(84);
   const [steps, setSteps] = useState<number>(2472);
   const [pulse, setPulse] = useState<number>(74);
   const { t } = useLanguage();
+
+  const maxWeight = 100;
+  const maxSteps = 2500;
+  const maxPulse = 150;
+
   return (
-    <div style={{ width: '85%', margin: 'auto', fontFamily: 'Open Sans' }}>
-      <Stack direction="column">
+    <div
+      style={{
+        width: '100%',
+        margin: 'auto',
+        fontFamily: 'Open Sans',
+        display: 'flex',
+      }}
+    >
+      <Stack direction="column" style={{ flex: 1, marginLeft: '10px' }}>
         <div style={{ padding: '2vh' }}></div>
         <Stack direction="row" sx={{ width: '60%', alignItems: 'center' }}>
           <Typography
@@ -22,8 +35,10 @@ export default function Home() {
           >
             {daysLeft}
           </Typography>
-
-          <Typography variant="h5" sx={{ lineHeight: '1', fontWeight: 'bold' }}>
+          <Typography
+            variant="h5"
+            sx={{ lineHeight: '1', fontWeight: 'bold', marginLeft: '10px' }}
+          >
             {t('days-left')}
           </Typography>
         </Stack>
@@ -31,7 +46,7 @@ export default function Home() {
           <Typography variant="caption" lineHeight={3}>
             {t('latest-result')}
           </Typography>
-          <Stack direction="row" spacing={2}>
+          <Stack direction="row" spacing={2} alignItems="center">
             <Scale />
             <Stack direction="column">
               <Typography sx={{ lineHeight: '1' }}>{weight}</Typography>
@@ -40,14 +55,14 @@ export default function Home() {
               </Typography>
             </Stack>
           </Stack>
-          <Stack direction="row" spacing={2}>
+          <Stack direction="row" spacing={2} alignItems="center">
             <Steps />
             <Stack direction="column">
               <Typography sx={{ lineHeight: '1' }}>{steps}</Typography>
               <Typography sx={{ lineHeight: '1' }}>{t('steps')}</Typography>
             </Stack>
           </Stack>
-          <Stack direction="row" spacing={2}>
+          <Stack direction="row" spacing={2} alignItems="center">
             <Pulse />
             <Stack direction="column">
               <Typography sx={{ lineHeight: '1' }}>{pulse}</Typography>
@@ -56,6 +71,42 @@ export default function Home() {
           </Stack>
         </Stack>
       </Stack>
+
+      <Stack
+        direction="column"
+        style={{
+          flex: 1,
+          marginLeft: '30px',
+          marginTop: '10px',
+          alignItems: 'center',
+        }}
+      >
+        <Stack direction="row" spacing={1}>
+          <ChartMeter
+            value={pulse}
+            label={<Pulse />}
+            barColor="hsla(19, 100%, 45%)"
+            lighterColor="hsla(19, 100%, 45%, 0.4)"
+            maxValue={maxPulse}
+          />
+          <ChartMeter
+            value={steps}
+            label={<Steps />}
+            barColor="hsla(200, 100%, 26%)"
+            lighterColor="hsla(200, 100%, 26%, 0.4)"
+            maxValue={maxSteps}
+          />
+          <ChartMeter
+            value={weight}
+            label={<Scale />}
+            barColor="hsla(196, 100%, 44%)"
+            lighterColor="hsla(196, 100%, 44%, 0.4)"
+            maxValue={maxWeight}
+          />
+        </Stack>
+      </Stack>
     </div>
   );
-}
+};
+
+export default Home;
