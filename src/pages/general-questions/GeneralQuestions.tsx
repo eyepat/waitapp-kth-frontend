45289b7659ec41d425d0f2aaf481f0febf29ce23
PatBtw <patkon101@gmail.com>
2024-07-07 02:47:20 +0200

    Reapply "Update GeneralQuestions.tsx"
    This reverts commit 8761e557edcb13cc928029e606b1656ffa6cf556.

--- src/pages/general-questions/GeneralQuestions.tsx
+++ src/pages/general-questions/GeneralQuestions.tsx
@@ -10,7 +10,9 @@ import {
   SelectChangeEvent,
   Stack,
   Typography,
+  ThemeProvider,
 } from '@mui/material';
+import theme from '../../components/Theme';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers';
@@ -35,157 +37,162 @@ export default function GeneralQuestions() {
     setSelectedGender(event.target.value);
   };
   return (
-    <Stack
-      direction="column"
-      alignItems="start"
-      padding="0 8vw 0 8vw"
-      maxWidth="1000px"
-      margin="auto"
-    >
-      <Typography variant="h5" marginBottom="1rem" alignSelf="center">
-        {t('questions-about-you')}
-      </Typography>
-      <Typography variant="caption" alignSelf="start" marginBottom="2vh">
-        {t('general-questions')}
-      </Typography>
-      <Typography fontWeight="bold">{'1. ' + t('gender')}</Typography>
-      <FormControl fullWidth>
-        <InputLabel>{t('select-gender')}</InputLabel>
-        <Select
-          value={selectedGender}
-          onChange={handleGenderChange}
-          fullWidth={true}
-          title={t('select-gender')}
-          label={t('select-gender')}
-        >
-          <MenuItem value="male">{t('male')}</MenuItem>
-          <MenuItem value="female">{t('female')}</MenuItem>
-          <MenuItem value="non-binary">{t('non-binary')}</MenuItem>
-          <MenuItem value="other">{t('other')}</MenuItem>
-        </Select>
-      </FormControl>
-      <Typography fontWeight="bold">{'2. ' + t('birth-year')}</Typography>
-      <DatePicker
-        sx={{ width: '100%' }}
-        label={t('select-birth-year')}
-        views={['year']}
-      />
-      <Stack direction="row" spacing={3} width="100%">
-        <Stack direction="column" width="100%">
-          <Typography fontWeight="bold">{'3. ' + t('length')}</Typography>
-          <FormControl fullWidth>
-            <InputLabel>...</InputLabel>
-            <OutlinedInput label={t('select-length')} endAdornment={'cm'} />
-          </FormControl>
-        </Stack>
-        <Stack direction="column" width="100%">
-          <Typography fontWeight="bold">{'4. ' + t('weight')}</Typography>
-          <FormControl fullWidth>
-            <InputLabel>...</InputLabel>
-            <OutlinedInput label={t('select-weight')} endAdornment={'kg'} />
-          </FormControl>
-        </Stack>
-      </Stack>
-      <Stack direction="row" spacing={3} width="100%">
-        <Stack direction="column" width="50%">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontWeight="bold">
-              {'5. ' + t('waist-measurement')}
-            </Typography>
-            <Button
-              sx={{ padding: 0, margin: 0, minWidth: 0 }}
-              onClick={handlePopUp}
-            >
-              <InfoOutlined />
-            </Button>
-          </Stack>
-          <FormControl fullWidth>
-            <InputLabel>...</InputLabel>
-            <OutlinedInput
-              label={t('select-waist-measurement')}
-              endAdornment={'cm'}
-            />
-          </FormControl>
-        </Stack>
-        <div style={{ width: '50%' }}></div>
-      </Stack>
-      <Stack direction="row" spacing={3} width="100%">
-        <Stack direction="column" width="50%">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontWeight="bold">
-              {'6. ' + t('resting-pulse')}
-            </Typography>
-            <Button
-              sx={{ padding: 0, margin: 0, minWidth: 0 }}
-              onClick={handlePopUp}
-            >
-              <InfoOutlined />
-            </Button>
-          </Stack>
-          <FormControl fullWidth>
-            <InputLabel>...</InputLabel>
-            <OutlinedInput
-              label={t('select-resting-pulse')}
-              endAdornment={'bpm'}
-            />
-          </FormControl>
-        </Stack>
-        <Stack direction="column" width="50%">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontWeight="bold">
-              {'7. ' + t('blood-pressure')}
-            </Typography>
-            <Button
-              sx={{ padding: 0, margin: 0, minWidth: 0 }}
-              onClick={handlePopUp}
-            >
-              <InfoOutlined />
-            </Button>
-          </Stack>
-          <FormControl fullWidth>
-            <InputLabel>...</InputLabel>
-            <OutlinedInput
-              label={t('select-blood-pressure')}
-              endAdornment={'mm/hg'}
-            />
-          </FormControl>
-        </Stack>
-      </Stack>
-      <div style={{ marginTop: '4vh', marginBottom: '4vh', width: '100%' }}>
-        <Typography fontWeight="bold">{t('ablation')}</Typography>
-        <DatePicker sx={{ width: '100%' }} label={t('select-ablation-date')} />
-      </div>
-      <Button
-        variant="contained"
-        sx={{
-          width: '80%',
-          margin: 'auto',
-          bgcolor: 'black',
-          borderRadius: '10px',
-          '&:hover': {
-            backgroundColor: '#333',
-          },
-        }}
+    <ThemeProvider theme={theme}>
+      <Stack
+        direction="column"
+        alignItems="start"
+        padding="0 8vw 0 8vw"
+        maxWidth="1000px"
+        margin="auto"
       >
-        {t('next')}
-      </Button>
-
-      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-        <Typography
-          variant="h5"
-          fontWeight="bold"
-          fontSize="20px"
-          marginTop="10px"
-          marginLeft="20px"
-        >
-          {t('why-we-need-this-data')}
+        <Typography variant="h5" marginBottom="1rem" alignSelf="center">
+          {t('questions-about-you')}
         </Typography>
-        <DialogContent>
-          <Typography textAlign="left" marginTop="-10px">
-            {t('why-we-need-this-data-text')}
+        <Typography variant="caption" alignSelf="start" marginBottom="2vh">
+          {t('general-questions')}
+        </Typography>
+        <Typography fontWeight="bold">{'1. ' + t('gender')}</Typography>
+        <FormControl fullWidth>
+          <InputLabel>{t('select-gender')}</InputLabel>
+          <Select
+            value={selectedGender}
+            onChange={handleGenderChange}
+            fullWidth={true}
+            title={t('select-gender')}
+            label={t('select-gender')}
+          >
+            <MenuItem value="male">{t('male')}</MenuItem>
+            <MenuItem value="female">{t('female')}</MenuItem>
+            <MenuItem value="non-binary">{t('non-binary')}</MenuItem>
+            <MenuItem value="other">{t('other')}</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography fontWeight="bold">{'2. ' + t('birth-year')}</Typography>
+        <DatePicker
+          sx={{ width: '100%' }}
+          label={t('select-birth-year')}
+          views={['year']}
+        />
+        <Stack direction="row" spacing={3} width="100%">
+          <Stack direction="column" width="100%">
+            <Typography fontWeight="bold">{'3. ' + t('length')}</Typography>
+            <FormControl fullWidth>
+              <InputLabel>...</InputLabel>
+              <OutlinedInput label={t('select-length')} endAdornment={'cm'} />
+            </FormControl>
+          </Stack>
+          <Stack direction="column" width="100%">
+            <Typography fontWeight="bold">{'4. ' + t('weight')}</Typography>
+            <FormControl fullWidth>
+              <InputLabel>...</InputLabel>
+              <OutlinedInput label={t('select-weight')} endAdornment={'kg'} />
+            </FormControl>
+          </Stack>
+        </Stack>
+        <Stack direction="row" spacing={3} width="100%">
+          <Stack direction="column" width="50%">
+            <Stack direction="row" justifyContent="space-between">
+              <Typography fontWeight="bold">
+                {'5. ' + t('waist-measurement')}
+              </Typography>
+              <Button
+                sx={{ padding: 0, margin: 0, minWidth: 0 }}
+                onClick={handlePopUp}
+              >
+                <InfoOutlined />
+              </Button>
+            </Stack>
+            <FormControl fullWidth>
+              <InputLabel>...</InputLabel>
+              <OutlinedInput
+                label={t('select-waist-measurement')}
+                endAdornment={'cm'}
+              />
+            </FormControl>
+          </Stack>
+          <div style={{ width: '50%' }}></div>
+        </Stack>
+        <Stack direction="row" spacing={3} width="100%">
+          <Stack direction="column" width="50%">
+            <Stack direction="row" justifyContent="space-between">
+              <Typography fontWeight="bold">
+                {'6. ' + t('resting-pulse')}
+              </Typography>
+              <Button
+                sx={{ padding: 0, margin: 0, minWidth: 0 }}
+                onClick={handlePopUp}
+              >
+                <InfoOutlined />
+              </Button>
+            </Stack>
+            <FormControl fullWidth>
+              <InputLabel>...</InputLabel>
+              <OutlinedInput
+                label={t('select-resting-pulse')}
+                endAdornment={'bpm'}
+              />
+            </FormControl>
+          </Stack>
+          <Stack direction="column" width="50%">
+            <Stack direction="row" justifyContent="space-between">
+              <Typography fontWeight="bold">
+                {'7. ' + t('blood-pressure')}
+              </Typography>
+              <Button
+                sx={{ padding: 0, margin: 0, minWidth: 0 }}
+                onClick={handlePopUp}
+              >
+                <InfoOutlined />
+              </Button>
+            </Stack>
+            <FormControl fullWidth>
+              <InputLabel>...</InputLabel>
+              <OutlinedInput
+                label={t('select-blood-pressure')}
+                endAdornment={'mm/hg'}
+              />
+            </FormControl>
+          </Stack>
+        </Stack>
+        <div style={{ marginTop: '4vh', marginBottom: '4vh', width: '100%' }}>
+          <Typography fontWeight="bold">{t('ablation')}</Typography>
+          <DatePicker
+            sx={{ width: '100%' }}
+            label={t('select-ablation-date')}
+          />
+        </div>
+        <Button
+          variant="contained"
+          sx={{
+            width: '80%',
+            margin: 'auto',
+            bgcolor: 'black',
+            borderRadius: '10px',
+            '&:hover': {
+              backgroundColor: '#333',
+            },
+          }}
+        >
+          {t('next')}
+        </Button>
+
+        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            fontSize="20px"
+            marginTop="10px"
+            marginLeft="20px"
+          >
+            {t('why-we-need-this-data')}
           </Typography>
-        </DialogContent>
-      </Dialog>
-    </Stack>
+          <DialogContent>
+            <Typography textAlign="left" marginTop="-10px">
+              {t('why-we-need-this-data-text')}
+            </Typography>
+          </DialogContent>
+        </Dialog>
+      </Stack>
+    </ThemeProvider>
   );
 }
