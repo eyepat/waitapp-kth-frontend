2508a06da5318dadd0b08770363510f0e5f1c006
Pat <patkon101@gmail.com>
2024-07-08 10:24:35 +0200

    chore: renamed  MenuButton

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material';
+import { styled } from '@mui/system';
+
+const MenuButton = styled(Button)({
+  borderRadius: '0 14px 14px 0',
+  width: '90vw',
+  maxWidth: '100%',
+  height: '50px',
+  color: 'black',
+  backgroundColor: 'white',
+  justifyContent: 'space-between',
+  '&:hover': {
+    backgroundColor: 'white',
+  },
+});
+
+export default MenuButton;
