17bfcd5785947893767d5e105dc7f704827e7a44
PatBtw <patkon101@gmail.com>
2024-07-12 15:44:53 +0200

    feat: Add RecipeCard component
    This commit adds a new RecipeCard component to the project. The RecipeCard component displays a recipe image, title, and a button. It is styled using Material-UI components and custom CSS. This addition enhances the user experience by providing a visually appealing way to showcase recipe information.

---
+++ src/components/Cards/RecipeCard.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import { BigArrow } from '../../utils/Icons';
+
+interface RecipeCardProps {
+  img: string;
+  title: string;
+}
+
+const RecipeCardButton = styled(Button)({
+  display: 'flex',
+  alignItems: 'center',
+  borderRadius: '12px',
+  width: '90vw',
+  maxWidth: '100%',
+  height: '100px',
+  color: 'black',
+  backgroundColor: 'white',
+  justifyContent: 'space-between',
+  padding: '0',
+  paddingRight: '10px',
+  boxSizing: 'border-box',
+  '&:hover': {
+    backgroundColor: 'white',
+  },
+});
+
+const RecipeImage = styled('img')({
+  width: '100px',
+  height: '100px',
+  borderRadius: '12px 0 0 12px',
+  objectFit: 'cover',
+});
+
+const RecipeCard: React.FC<RecipeCardProps> = ({ img, title }) => {
+  return (
+    <RecipeCardButton>
+      <RecipeImage src={img} alt={title} />
+      <Typography
+        variant="body1"
+        fontSize="19px"
+        fontWeight="bold"
+        style={{ flexGrow: 1, marginLeft: '35px', textAlign: 'left' }}
+      >
+        {title}
+      </Typography>
+      <BigArrow />
+    </RecipeCardButton>
+  );
+};
+
+export default RecipeCard;
