6e1f339725823a4a9abbc507f2ad76e40c42ae97
PatBtw <patkon101@gmail.com>
2024-07-27 14:35:50 +0200

    chore: Add SVG icons for changing, pausing, and stopping sprints

--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -741,3 +741,70 @@ export function Contact() {
     </svg>
   );
 }
+
+export function StopSprint() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="8.5" stroke="black" />
+      <rect
+        x="8.5"
+        y="8.5"
+        width="7"
+        height="7"
+        rx="0.5"
+        fill="black"
+        stroke="black"
+      />
+    </svg>
+  );
+}
+
+export function PauseSprint() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="8.5" stroke="black" />
+      <rect x="8.25" y="8" width="2.5" height="8" rx="0.5" fill="black" />
+      <path
+        d="M13.25 8.5C13.25 8.22386 13.4739 8 13.75 8H15.25C15.5261 8 15.75 8.22386 15.75 8.5V15.5C15.75 15.7761 15.5261 16 15.25 16H13.75C13.4739 16 13.25 15.7761 13.25 15.5V8.5Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
+
+export function ChangeSprint() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14 15L10 19L14 23" stroke="black" />
+      <path
+        d="M18.0622 8.5C18.7138 9.62862 19.0374 10.9167 18.9966 12.2193C18.9557 13.5219 18.5521 14.7872 17.8311 15.8728C17.11 16.9584 16.1003 17.8212 14.9155 18.364C13.7307 18.9067 12.4179 19.108 11.1249 18.9451"
+        stroke="black"
+        stroke-linecap="round"
+      />
+      <path d="M10 9L14 5L10 1" stroke="black" />
+      <path
+        d="M5.93782 15.5C5.27676 14.355 4.95347 13.0462 5.0054 11.7251C5.05733 10.404 5.48234 9.12457 6.23124 8.03498C6.98013 6.9454 8.02229 6.09019 9.23708 5.56834C10.4519 5.04649 11.7896 4.87934 13.0955 5.08625"
+        stroke="black"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
