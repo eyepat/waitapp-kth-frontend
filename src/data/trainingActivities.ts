d896f7a7183935df90fa1548d62b37fc86c709af
Philip Zingmark <philip.zingmark@gmail.com>
2024-08-08 08:59:58 +0200

    fixed some typos in sprint and added translations to dummy trainingActivities

--- src/data/trainingActivities.ts
+++ src/data/trainingActivities.ts
@@ -3,8 +3,8 @@ import { TrainingActivity } from '../types';
 export const trainingActivities: { [key: string]: TrainingActivity } = {
   //Just a placeholder for test
   '1-1-1': {
-    title: 'Promenad',
-    description: 'Prominera enkelt på stället.',
+    title: 'training-sprint-placeholder-title',
+    description: 'training-sprint-placeholder-desc',
     videoUrl: 'https://www.youtube.com/embed/xtof9tAakkE',
   },
   // Add more activities for each combination of day, rapa, and week
