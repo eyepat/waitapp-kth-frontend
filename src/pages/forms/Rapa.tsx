946ac77656cd0ec77cce1eefd8747e4defe1e12f
PatBtw <patkon101@gmail.com>
2024-08-25 19:30:11 +0200

    chore: Update RapaForm page layout and add navigation
    The commit updates the layout of the RapaForm page in the settings/forms section. It also adds navigation to the page, allowing users to fill out a form related to Rapa.

--- src/pages/forms/Rapa.tsx
+++ src/pages/forms/Rapa.tsx
@@ -2,9 +2,6 @@ import {
   Button,
   Dialog,
   DialogContent,
-  List,
-  ListItem,
-  ListItemText,
   Stack,
   ThemeProvider,
   Typography,
@@ -12,9 +9,11 @@ import {
 import theme from '../../components/Theme';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Rapa() {
   const { t } = useLanguage();
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
 
   function handleShowExamples(): void {
@@ -80,33 +79,129 @@ export default function Rapa() {
           {t('examples')}
         </Button>
 
-        <Button variant="contained" sx={{ width: '50%' }}>
+        <Button
+          variant="contained"
+          onClick={() => navigate('questions')}
+          sx={{ width: '50%' }}
+        >
           {t('begin')}
         </Button>
       </Stack>
 
-      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="md"
+        fullWidth
+        sx={{ marginBottom: '5vh' }}
+      >
         <Typography
           variant="h5"
           textAlign="center"
           fontWeight="bold"
-          marginTop="10px"
+          padding="0.3rem"
+          marginTop="1rem"
         >
           {t('phyiscal-activities-intensity-levels-examples')}
         </Typography>
 
         <DialogContent>
-          <Stack direction="column" spacing={2} padding={1} border={1}>
-            <Stack direction="row">
-              <Typography>light acitivies</Typography>{' '}
+          <Stack direction="column" spacing={2} padding={1}>
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              padding={2}
+              border={1}
+              borderRadius={2}
+              borderColor="grey.500"
+            >
+              <Stack direction="column" spacing={1} flex={1}>
+                <Typography variant="h6" fontWeight="bold">
+                  {t('light-activities')}
+                </Typography>
+                <Typography>• {t('light-activity-description1')}</Typography>
+                <Typography>• {t('light-activity-description2')}</Typography>
+              </Stack>
+              <Stack
+                direction="column"
+                spacing={1}
+                flex={1}
+                paddingLeft={2}
+                borderLeft={1}
+                borderColor="grey.500"
+                marginTop={{ xs: '16px', sm: '0' }}
+              >
+                <Typography variant="body2" fontWeight="bold">
+                  {t('examples')}:
+                </Typography>
+                <Typography>• {t('light-activity-example1')}</Typography>
+                <Typography>• {t('light-activity-example2')}</Typography>
+                <Typography>• {t('light-activity-example3')}</Typography>
+              </Stack>
             </Stack>
-            <Stack direction="row">
-              {' '}
-              <Typography>moderate acitivies</Typography>{' '}
+
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              padding={2}
+              border={1}
+              borderRadius={2}
+              borderColor="grey.500"
+            >
+              <Stack direction="column" spacing={1} flex={1}>
+                <Typography variant="h6" fontWeight="bold">
+                  {t('moderate-activities')}
+                </Typography>
+                <Typography>• {t('moderate-activity-description1')}</Typography>
+                <Typography>• {t('moderate-activity-description2')}</Typography>
+              </Stack>
+              <Stack
+                direction="column"
+                spacing={1}
+                flex={1}
+                paddingLeft={2}
+                borderLeft={1}
+                borderColor="grey.500"
+                marginTop={{ xs: '16px', sm: '0' }}
+              >
+                <Typography variant="body2" fontWeight="bold">
+                  {t('examples')}:
+                </Typography>
+                <Typography>• {t('moderate-activity-example1')}</Typography>
+                <Typography>• {t('moderate-activity-example2')}</Typography>
+                <Typography>• {t('moderate-activity-example3')}</Typography>
+                <Typography>• {t('moderate-activity-example4')}</Typography>
+              </Stack>
             </Stack>
-            <Stack direction="row">
-              {' '}
-              <Typography>vigorous acitivies</Typography>{' '}
+
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              padding={2}
+              border={1}
+              borderRadius={2}
+              borderColor="grey.500"
+            >
+              <Stack direction="column" spacing={1} flex={1}>
+                <Typography variant="h6" fontWeight="bold">
+                  {t('vigorous-activities')}
+                </Typography>
+                <Typography>• {t('vigorous-activity-description1')}</Typography>
+                <Typography>• {t('vigorous-activity-description2')}</Typography>
+              </Stack>
+              <Stack
+                direction="column"
+                spacing={1}
+                flex={1}
+                paddingLeft={2}
+                borderLeft={1}
+                borderColor="grey.500"
+                marginTop={{ xs: '16px', sm: '0' }}
+              >
+                <Typography variant="body2" fontWeight="bold">
+                  {t('examples')}:
+                </Typography>
+                <Typography>• {t('vigorous-activity-example1')}</Typography>
+                <Typography>• {t('vigorous-activity-example2')}</Typography>
+                <Typography>• {t('vigorous-activity-example3')}</Typography>
+              </Stack>
             </Stack>
           </Stack>
         </DialogContent>
