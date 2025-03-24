73f9de2d19d9077b0568840cda80674d43c2ddac
Martin Jacobsson <marjacob@kth.se>
2025-03-24 23:54:47 +0000

    Calculate targets.

--- src/components/Cards/MesurementsCard.tsx
+++ src/components/Cards/MesurementsCard.tsx
@@ -1,26 +1,50 @@
 import { useEffect } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
 import { Card, CardContent, Typography, Grid, Stack } from '@mui/material';
 import StraightenIcon from '@mui/icons-material/Straighten';
 import {
   useBloodPressureContext,
   useWaistSizeContext,
   useWeightContext,
+  useHeightContext,
 } from '../../contexts/MetricsContext';
+import { Gender } from '../../api/BaseClient';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { BloodPressure, Scale } from '../../utils/Icons';
 
 const MeasurementCard = () => {
   const { t } = useLanguage();
+  const { user } = useAuth();
   const { getLatest: getBP, latest: bpLatest } = useBloodPressureContext();
   const { getLatest: getWeight, latest: weightLatest } = useWeightContext();
+  const { getResources: getWeights, resources: weights } = useWeightContext();
+  const { getLatest: getHeight, latest: height } = useHeightContext();
   const { getLatest: getWaistSize, latest: waistSizeLatest } =
     useWaistSizeContext();
 
   useEffect(() => {
     if (!bpLatest) getBP();
     if (!weightLatest) getWeight();
+    if (!weights) getWeights();
+    if (!height) getHeight();
     if (!waistSizeLatest) getWaistSize();
-  }, [bpLatest, weightLatest, waistSizeLatest]);
+  }, [bpLatest, weightLatest, weights, height, waistSizeLatest]);
+
+  const calcTargetWeight = (
+    firstWeight: number | undefined,
+    height: number | undefined
+  ): number | undefined => {
+    if (!firstWeight || !height) return undefined;
+    const target_weight = (27 * height * height) / 10000;
+    if (target_weight < firstWeight * 0.9) return Math.round(firstWeight * 0.9);
+    return Math.round(target_weight);
+  };
+
+  const calcTargetWaist = (gender: Gender | undefined): number | undefined => {
+    if (!gender) return undefined;
+    if (gender == 'MALE') return 102;
+    return 88;
+  };
 
   return (
     <Card
@@ -55,7 +79,10 @@ const MeasurementCard = () => {
                 <Typography variant="body1">
                   {t('weight')}: {weightLatest.value} kg
                 </Typography>
-                <Typography variant="caption">{t('target')}: 60 kg</Typography>
+                <Typography variant="caption">
+                  {t('target')}:{' '}
+                  {calcTargetWeight(weights?.at(0)?.value, height?.value)} kg
+                </Typography>
               </Stack>
             </Grid>
           )}
@@ -67,7 +94,7 @@ const MeasurementCard = () => {
                   {t('waist-measurement')}: {waistSizeLatest.value} cm
                 </Typography>
                 <Typography variant="caption">
-                  {t('target')}: 88/102 cm
+                  {t('target')}: {calcTargetWaist(user?.gender)} cm
                 </Typography>
               </Stack>
             </Grid>
