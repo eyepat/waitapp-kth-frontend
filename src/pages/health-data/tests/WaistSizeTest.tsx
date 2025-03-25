cf2944f600e152266058aa2ff0b9344defea9800
Martin Jacobsson <marjacob@kth.se>
2025-03-25 11:27:28 +0000

    Instructions to measuring waist size.

--- src/pages/health-data/tests/WaistSizeTest.tsx
+++ src/pages/health-data/tests/WaistSizeTest.tsx
@@ -1,6 +1,8 @@
 import {
   Box,
   Button,
+  Dialog,
+  DialogContent,
   InputAdornment,
   Stack,
   TextField,
@@ -8,9 +10,11 @@ import {
   Typography,
   styled,
 } from '@mui/material';
+import waistsize from '../../../assets/tests/waistsize.svg';
 import { useLanguage } from '../../../contexts/LanguageContext';
 import theme from '../../../components/Theme';
 import { useAuth } from '../../../contexts/AuthContext';
+import { Svg } from '../../../utils/Icons';
 import { useSnackbar } from 'notistack';
 import { useSprintContext } from '../../../contexts/SprintContext';
 import { useState } from 'react';
@@ -49,9 +53,18 @@ export default function WaistSizeTest() {
   const { sprint } = useSprintContext();
   const { createResource } = useWaistSizeContext();
   const [waistSize, setWaistSize] = useState<string>('');
-
   const { t } = useLanguage();
   const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+
+  function handleHowToMeasure(): void {
+    setOpen(true);
+  }
+
+  function handleClose(): void {
+    setOpen(false);
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -63,97 +76,150 @@ export default function WaistSizeTest() {
           {t('about-waist-size')}
         </Typography>
 
-        <Box
-          marginTop="100px"
-          bottom="15%"
-          width="100%"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
+        <Stack
+          sx={{
+            bottom: '10%',
+            width: '100%',
+          }}
         >
-          <CustomInputField
-            type="number"
-            variant="outlined"
-            value={waistSize}
-            onChange={(e) => {
-              if (e.target.value.length <= 5) {
-                setWaistSize(e.target.value);
-              }
-            }}
-            inputProps={{ maxLength: 4 }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Typography
-                    fontWeight="bold"
-                    fontSize="large"
-                    color="hsla(0, 0%, 20%, 1)"
-                  >
-                    cm
-                  </Typography>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{
-              backgroundColor: 'black',
-              marginTop: '100px',
-              borderRadius: '10px',
-              width: '60%',
-              height: '50px',
-              '&:active, &:focus': {
+          <Box
+            marginTop="100px"
+            bottom="15%"
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <CustomInputField
+              type="number"
+              variant="outlined"
+              value={waistSize}
+              onChange={(e) => {
+                if (e.target.value.length <= 5) {
+                  setWaistSize(e.target.value);
+                }
+              }}
+              inputProps={{ maxLength: 4 }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Typography
+                      fontWeight="bold"
+                      fontSize="large"
+                      color="hsla(0, 0%, 20%, 1)"
+                    >
+                      cm
+                    </Typography>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{
+                borderRadius: '10px',
+                marginTop: '90px',
+                width: '60%',
+                color: 'black',
+                borderColor: 'black',
+                borderWidth: '2px',
+                '&:hover': {
+                  borderColor: 'black',
+                  borderWidth: '2px',
+                },
+              }}
+              onClick={handleHowToMeasure}
+            >
+              <Typography>{t('how-to-measure')}</Typography>
+            </Button>
+
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
                 backgroundColor: 'black',
-              },
-              '&:hover': {
-                backgroundColor: '#333',
-              },
-            }}
-            onClick={() => {
-              if (user === undefined || user.id === undefined) {
-                enqueueSnackbar('error-no-user', { variant: 'error' });
-                return;
-              }
-              if (!/^\d+(\.\d+)?$/.test(waistSize)) {
-                enqueueSnackbar('invalid-waist-size-error', {
-                  variant: 'error',
-                });
-                return;
-              }
-              const waistNumber = Number(waistSize);
-              if (isNaN(waistNumber) || waistNumber < 40 || waistNumber > 200) {
-                enqueueSnackbar('invalid-waist-size-error', {
-                  variant: 'error',
-                });
-                return;
-              }
-
-              const metric: WaistSizeDTO = {
-                userID: user?.id,
-                sprintID: sprint?.id!,
-                timestamp: dayjs().format('YYYY-MM-DDTHH:mm:ss'),
-                value: waistNumber,
-              };
-
-              createResource(metric)
-                .then(() => {
-                  enqueueSnackbar('success-adding-waist-size', {
-                    variant: 'success',
-                  });
-                  navigate('/health-data/overview');
-                })
-                .catch(() => {
-                  enqueueSnackbar('error-adding-waist-size', {
+                marginTop: '10px',
+                borderRadius: '10px',
+                width: '60%',
+                height: '50px',
+                '&:active, &:focus': {
+                  backgroundColor: 'black',
+                },
+                '&:hover': {
+                  backgroundColor: '#333',
+                },
+              }}
+              onClick={() => {
+                if (user === undefined || user.id === undefined) {
+                  enqueueSnackbar('error-no-user', { variant: 'error' });
+                  return;
+                }
+                if (!/^\d+(\.\d+)?$/.test(waistSize)) {
+                  enqueueSnackbar('invalid-waist-size-error', {
                     variant: 'error',
                   });
-                });
-            }}
+                  return;
+                }
+                const waistNumber = Number(waistSize);
+                if (
+                  isNaN(waistNumber) ||
+                  waistNumber < 40 ||
+                  waistNumber > 200
+                ) {
+                  enqueueSnackbar('invalid-waist-size-error', {
+                    variant: 'error',
+                  });
+                  return;
+                }
+
+                const metric: WaistSizeDTO = {
+                  userID: user?.id,
+                  sprintID: sprint?.id!,
+                  timestamp: dayjs().format('YYYY-MM-DDTHH:mm:ss'),
+                  value: waistNumber,
+                };
+
+                createResource(metric)
+                  .then(() => {
+                    enqueueSnackbar('success-adding-waist-size', {
+                      variant: 'success',
+                    });
+                    navigate('/health-data/overview');
+                  })
+                  .catch(() => {
+                    enqueueSnackbar('error-adding-waist-size', {
+                      variant: 'error',
+                    });
+                  });
+              }}
+            >
+              <Typography>{t('save')}</Typography>
+            </Button>
+          </Box>
+        </Stack>
+
+        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+          <Svg src={waistsize} />
+          <Typography
+            variant="h5"
+            textAlign="center"
+            fontWeight="bold"
+            marginTop="10px"
           >
-            <Typography>{t('save')}</Typography>
-          </Button>
-        </Box>
+            {t('how-do-i-measure')} {t('waist-size')}
+          </Typography>
+          <DialogContent>
+            <Typography
+              variant="subtitle2"
+              textAlign="justify"
+              marginTop="-10px"
+            >
+              {t('how-to-measure-waist-size')}
+            </Typography>
+          </DialogContent>
+        </Dialog>
       </Stack>
     </ThemeProvider>
   );
